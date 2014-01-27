cf732d94690dff0a96c66dc9ba11a953e3c17ddf
baccka <a.lorenz1@nuigalway.ie>
2014-01-27 19:17:49 +0000

    implement Hello World with visited times state

--- app.js
+++ app.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var app = express();
 
-app.get('/', function(request, response) {
-  response.send("Hello World!");
-});
+var times = [];
 
-app.get('/api', function(request, response) {
-  response.send({name: "Raymond", age: 40});
+app.get('/', function(request, response) {
+  // Record the new access time
+  times.push(new Date());
+  
+  var message = "Hello world!<br>Visited at:<br>";
+  for(var i = 0; i < times.length; ++i) {
+    message += times[i];
+    message += "<br>";
+  }
+  response.send(message);
 });
 
 app.listen(8625);
